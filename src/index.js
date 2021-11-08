7d8e465d24d5f506c736a3109ae3f56748e83eac
Jake <jakerfilut@gmail.com>
2021-11-08 14:57:01 -0700

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault()
+      console.log(e)
+  })
+  
+document.getElementById("Button").onclick = function(){
+const newHeader = document.createElement("h2");
+newHeader.textContent = newtaskdescription.value;
+const fullList = document.getElementById("list")
+fullList.append(newHeader)
+}
+
 });
+
+// document.querySelector('form').addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   console.log(e)
+// })
+// });
